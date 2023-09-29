1dffbd7a67a711d0f06cce54771d7007e94aeca2
Marcin <marcin.szturminski@gmail.com>
2023-09-29 19:16:10 +0200

    add, remove, modal added to page

--- js/main.js
+++ js/main.js
@@ -4,8 +4,43 @@ const allBtn = document.querySelectorAll('.btn')
 const boxToDo = document.querySelector('.box__todo')
 const allToDo = document.querySelectorAll('.todo')
 const boxTrash = document.querySelector('.box__trash')
+const wrapper = document.querySelector('.wrapper').children
+const modal = document.querySelector('.modal')
+const closeIcon = document.querySelector('.icon__close')
 
-// console.log(allToDo)
+// Adding ToDo
+
+const addToDoFunction = () => {
+	addFilterFunction()
+	showModalFunction()
+}
+
+const addFilterFunction = () => {
+	for (i = 0; i < wrapper.length - 1; i++) {
+		wrapper[i].classList.add('blur')
+	}
+}
+
+const showModalFunction = () => {
+	modal.classList.add('show-modal')
+}
+
+// Canceling ToDo
+
+const cancelToDoFunction = () => {
+	removeBlurFunction()
+	hideModalFunction()
+}
+
+const removeBlurFunction = () => {
+	for (i = 0; i < wrapper.length - 1; i++) {
+		wrapper[i].classList.remove('blur')
+	}
+}
+
+const hideModalFunction = () => {
+	modal.classList.remove('show-modal')
+}
 
 // This can be removed after all ToDo list is finished, this is only adding listeners to existing ToDo's. Since there will be not at the begining we can easily delete that
 
@@ -21,7 +56,8 @@ for (let i = 0; i < allCheckbox.length; i++) {
 
 // Function for creating ToDo
 
-const createToDoFunciton = () => {
+const createToDoFunction = () => {
+	const modalInputValue = document.querySelector('.modal__input').value
 	// Creating ToDo
 	const toDoDiv = document.createElement('div')
 	toDoDiv.classList.add('todo')
@@ -31,7 +67,8 @@ const createToDoFunciton = () => {
 	toDoInput.type = 'checkbox'
 	const toDoP = document.createElement('p')
 	toDoP.classList.add('todo__text')
-	toDoP.textContent = 'To do dodany przez button'
+	// toDoP.textContent = 'To do dodany przez button'
+	toDoP.textContent = modalInputValue
 	toDoDiv.appendChild(toDoInput)
 	toDoDiv.appendChild(toDoP)
 	boxToDo.appendChild(toDoDiv)
@@ -60,11 +97,13 @@ const createToDoFunciton = () => {
 	toDoDiv.addEventListener('dragend', () => {
 		toDoDiv.classList.remove('is-dragged')
 	})
+
+	cancelToDoFunction()
 }
 
 // Fuinction for removing last element
 
-const removeLastToDo = () => {
+const removeLastToDoFunction = () => {
 	boxToDo.removeChild(boxToDo.lastChild)
 }
 
@@ -90,5 +129,9 @@ boxTrash.addEventListener('dragover', e => {
 	boxTrash.removeChild(boxTrash.lastChild)
 })
 
-allBtn[0].addEventListener('click', createToDoFunciton)
-allBtn[1].addEventListener('click', removeLastToDo)
+// allBtn[0].addEventListener('click', createToDoFunciton)
+
+allBtn[0].addEventListener('click', addToDoFunction)
+allBtn[1].addEventListener('click', removeLastToDoFunction)
+closeIcon.addEventListener('click', cancelToDoFunction)
+allBtn[3].addEventListener('click', createToDoFunction)
